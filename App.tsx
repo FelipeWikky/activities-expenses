802ef7699b9e0f8bbb0885ea3133766d5545f4cf
Felipe Lima <felipewikky@gmail.com>
2022-03-26 18:18:16 -0300

    Libraries and resources configuration

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { ThemeProvider } from 'styled-components/native';
+import { THEME } from './src/theme';
+import { Routes } from './src/routes';
+
+export default function App() {
+  let [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={THEME}>
+      <Routes />
+      <StatusBar style="auto" />
+    </ThemeProvider>
+  );
+}
