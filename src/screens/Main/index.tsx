521b45effee29f4e55abbc5db535c0fe7f506872
Felipe Lima <felipewikky@gmail.com>
2022-03-26 23:09:01 -0300

    Added main screen with list of expenses

---
+++ src/screens/Main/index.tsx
@@ -0,0 +1,12 @@
+import { Container } from "./styles";
+import { ListItem } from "../../components/Expense/ListItem";
+
+const MainScreen: React.FC = () => {
+    return (
+        <Container>
+            <ListItem />
+        </Container>
+    );
+}
+
+export default MainScreen;
