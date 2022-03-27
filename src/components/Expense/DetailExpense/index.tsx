bd0c31ab236fc8504b135dc8177edb7fb5e7ea19
Felipe Lima <felipewikky@gmail.com>
2022-03-27 19:22:10 -0300

    Adde comment input and showing dates

--- src/components/Expense/DetailExpense/index.tsx
+++ src/components/Expense/DetailExpense/index.tsx
@@ -4,7 +4,6 @@ import { Dimensions } from "react-native";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import * as Random from 'expo-random';
 
 import { Modal, Container, Content, Header, Title, Description } from "./styles";
 
@@ -16,8 +15,9 @@ import { Checkbox } from "../../Checkbox";
 
 import { ExpenseItem } from "../../../types/models/expenseItem";
 import { getPercentageValue } from "../../../utils";
-import { formatDate, formatDateTime } from "../../../utils/format";
+import { formatDateTime } from "../../../utils/format";
 import { Input } from "../../Input";
+import { DatePicker } from "../../DatePicker";
 
 export type DetailExpenseHandles = {
     open: () => void;
@@ -30,6 +30,7 @@ type DetailExpenseProps = {
 
     onSave?: (item?: ExpenseItem) => void;
     onCancel?: (item?: ExpenseItem) => void;
+    onCloseModal?: () => void;
 }
 
 const schema = yup.object().shape({
@@ -37,25 +38,22 @@ const schema = yup.object().shape({
     description: yup.string().required("Este campo é obrigatório").min(3, "Mínimo de 3 caracteres"),
 });
 
-const DetailExpenseComponent: React.ForwardRefRenderFunction<DetailExpenseHandles, DetailExpenseProps> = (props, ref) => {
-
-    const { onlyView = true, onSave, onCancel } = props;
+const DetailExpenseComponent: React.ForwardRefRenderFunction<DetailExpenseHandles, DetailExpenseProps> = ({ onlyView = true, onSave, onCancel, onCloseModal, data }, ref) => {
     const modalizeRef = useRef<Modalize>(null);
 
-    const { control, handleSubmit, setValue, reset, getValues, formState: {  errors, } } = useForm<ExpenseItem>({
+    const { control, handleSubmit, setValue, reset, getValues, formState: { errors, } } = useForm<ExpenseItem>({
         resolver: yupResolver(schema),
-        defaultValues: props?.data,
+        defaultValues: data,
         shouldFocusError: true,
     });
 
     useEffect(() => {
-        console.log('props.data ', props?.data)
-        if (props?.data) {
-            for (const key of Object.keys(props.data)) {
-                setValue(key as any, props.data[key]);
+        if (data) {
+            for (const key of Object.keys(data)) {
+                setValue(key as any, data[key]);
             }
         }
-    }, [props?.data]);
+    }, [data]);
 
     const open = useCallback(() => {
         modalizeRef.current.open();
@@ -72,21 +70,14 @@ const DetailExpenseComponent: React.ForwardRefRenderFunction<DetailExpenseHandle
 
     const MODAL_HEIGHT = useMemo(() => getPercentageValue(Dimensions.get('window').height, 20), [Dimensions]);
 
-    const dateFormatted = useMemo(() => {
-        const createdAt = props?.data?.createdAt;
-        if (createdAt) return formatDateTime(createdAt);
-        return 'dd/mm/yyyy';
-    }, [props?.data]);
-
     const idFormatted = useMemo(() => {
-        const id = props?.data?.id;
+        const id = data?.id;
         if (id) return `# ${id}`;
         return '';
-    }, [props?.data]);
+    }, [data]);
 
     const onSavePress = useCallback((data: ExpenseItem) => {
         if (onSave) {
-            data.createdAt = new Date();
             onSave(data);
             resetFields();
         }
@@ -94,7 +85,7 @@ const DetailExpenseComponent: React.ForwardRefRenderFunction<DetailExpenseHandle
 
     const resetFields = () => {
         reset({
-            id: '', title: '', description: '', comment: '', finished: false, hasError: false, createdAt: ''
+            id: '', title: '', description: '', comment: '', finished: false, hasError: false, createdAt: '', when: ''
         });
     }
 
@@ -105,11 +96,20 @@ const DetailExpenseComponent: React.ForwardRefRenderFunction<DetailExpenseHandle
         resetFields();
     }, []);
 
+    const createdAsSameUpdated = useMemo(() => {
+        if (data?.createdAt === data?.updatedAt) return true;
+        return false;
+    }, [data?.createdAt, data?.updatedAt]);
+
     return (
         <Modal
             ref={modalizeRef}
             modalHeight={MODAL_HEIGHT}
-            onClosed={() => onCancelPress(false)}
+
+            onClosed={() => {
+                if (onCloseModal) onCloseModal();
+                resetFields();
+            }}
             scrollViewProps={{
                 showsHorizontalScrollIndicator: false,
                 showsVerticalScrollIndicator: false,
@@ -121,7 +121,7 @@ const DetailExpenseComponent: React.ForwardRefRenderFunction<DetailExpenseHandle
                 <Header showSave={!onlyView}>
                     {!onlyView && (
                         <Button
-                            type="SUCCESS" text="Salvar" textSize="NORMAL_SMALL"
+                            type="SUCCESS" text={data?.id ? "Atualizar" : "Salvar"} textSize="NORMAL_SMALL"
                             onPress={handleSubmit(onSavePress)}
                         />
                     )}
@@ -145,10 +145,16 @@ const DetailExpenseComponent: React.ForwardRefRenderFunction<DetailExpenseHandle
 
                     <Box>
                         <Input
-                            control={control} type="text" name="description" label="Descrição" 
+                            control={control} type="text" name="description" label="Descrição"
                             placeholder="Uma breve descrição da atividade"
                             error={errors?.description?.message}
                         />
+                    </Box>
+
+                    <Line shadow={2} marginVertical={12} />
+
+                    <Box>
+                        <DatePicker type="datetime" name="when" control={control} label="Para quando?" />
                     </Box>
 
                     <Line shadow={2} marginVertical={12} />
@@ -185,24 +191,36 @@ const DetailExpenseComponent: React.ForwardRefRenderFunction<DetailExpenseHandle
                     <Line shadow={2} marginVertical={12} />
 
                     <Box>
-                        <Title>
-                            Comentários
-                        </Title>
-                        <Description>
-                            {getValues("comment")}
-                        </Description>
+                        <Input
+                            control={control} type="text" name="comment" label="Comentário"
+                            placeholder="Algum comentário para este item?"
+                            error={errors?.comment?.message}
+                        />
                     </Box>
 
                     <Line shadow={2} marginVertical={12} />
 
-                    <Box direction="row" alignItems="center">
+                    <Box direction="row" alignItems="center" justifyContent="space-between">
                         <Title>
-                            Criado em
+                            {!!(data && data.createdAt) ? "Criado em " : "Criando em"}
                         </Title>
                         <Description style={{ marginBottom: 2 }}>
-                            {dateFormatted}
+                            {formatDateTime(data?.createdAt || new Date())}
                         </Description>
                     </Box>
+
+                    {!!(data?.updatedAt) && <Line shadow={2} marginVertical={12} />}
+
+                    {!createdAsSameUpdated && (
+                        <Box direction="row" alignItems="center" justifyContent="space-between">
+                            <Title>
+                                Atualizado em
+                            </Title>
+                            <Description style={{ marginBottom: 2 }}>
+                                {formatDateTime(data?.updatedAt)}
+                            </Description>
+                        </Box>
+                    )}
 
                 </Content>
             </Container>
--- src/types/models/expenseItem.ts
+++ src/types/models/expenseItem.ts
@@ -1,9 +1,42 @@
 export interface ExpenseItem {
+    /**
+     * Unique property for identification of object
+     */
     id?: string | number;
+    /**
+     * Title of item
+     */
     title: string;
+    /**
+     * Description of item
+     */
     description?: string;
+    /**
+     * Is used to indicate of item has finished
+     */
     finished?: boolean;
+    /**
+     * Is used to indicate of item has a problem or error
+     */
     hasError?: boolean;
+    /**
+     * Any comment for this expense item
+     */
     comment?: string;
+    /**
+     * Created date on storage
+     */
     createdAt?: string | Date;
+    /**
+     * Update date on storage
+     */
+    updatedAt?: string | Date;
+    /**
+     * Represente any date to indicate when this item has occurred
+     */
+    when?: string | Date; 
+    /**
+     * Indicate if this expense item is monthly
+     */
+    isMonthly?: boolean;
 }
