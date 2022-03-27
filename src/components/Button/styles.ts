11ad4ba4c1c16fdc6e3777e9c587f07f0be54a43
Felipe Lima <felipewikky@gmail.com>
2022-03-26 23:09:24 -0300

    Fix sizes on Button component

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,9 +1,12 @@
 import styled from 'styled-components/native';
 import { getColorByType } from '../../utils/colors';
+import { Constants } from '../../utils/constants';
 import { ButtonTextProps} from './types';
 
 
-export const StyledButton = styled.TouchableOpacity<ButtonTextProps>`
+export const StyledButton = styled.TouchableOpacity.attrs(props => ({
+    activeOpacity: Constants.BUTTON_CLICK_OPACITY
+}))<ButtonTextProps>`
     background-color: ${(props) => getColorByType(props.type)};
     border-radius: 50px;
     padding: 10px 30px;
@@ -11,10 +14,8 @@ export const StyledButton = styled.TouchableOpacity<ButtonTextProps>`
     justify-content: center;
 `;
 
-
 export const StyledText = styled.Text<ButtonTextProps>`
     color: ${({ theme }) => theme.COLORS.WHITE};
     font-family: ${({ theme }) => theme.FONTS.BOLD};
-    font-size: ${({ theme }) => theme.SIZE.NORMAL}px;
+    font-size: ${({ theme, textSize }) => textSize ? theme.SIZE[textSize] : theme.SIZE.NORMAL}px;
 `;
-
--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,4 +1,5 @@
 import { TouchableOpacityProps } from "react-native";
+import { THEME } from "../../theme";
 
 export enum ButtonType {
     DEFAULT = 'DEFAULT', SUCCESS = 'SUCCESS', DANGER = 'DANGER',
@@ -19,5 +20,6 @@ export enum ButtonSize {
 export type ButtonTextProps = TouchableOpacityProps & {
     text?: string;
     size?: keyof typeof ButtonSize;
+    textSize?: keyof typeof THEME.SIZE;
     type?: keyof typeof ButtonType;
 }
