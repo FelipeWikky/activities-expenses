0827d8d0340425527cb6c68b976c37b6ba675593
Felipe Lima <felipewikky@gmail.com>
2022-03-27 00:01:29 -0300

    Added new types of buttons with colors

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -2,14 +2,21 @@ import { ButtonType, ButtonState } from "../components/Button/types";
 import { THEME } from "../theme";
 
 export const getColorByType = (type: keyof typeof ButtonType, state = ButtonState.NORMAL) => {
-    switch(type) {
-        case 'SUCCESS': return THEME.COLORS.SUCCESS;
-        case 'DANGER': return THEME.COLORS.DANGER;
-        case 'DEFAULT': return THEME.COLORS.DEFAULT;
-        default: return THEME.COLORS.DEFAULT;
+    switch (type) {
+        case 'SUCCESS':
+        case 'SUCCESS_DISABLE':
+        case 'DANGER':
+        case 'DANGER_DISABLE':
+        case 'DEFAULT':
+        case 'DEFAULT_DISABLE':
+            return THEME.COLORS[type];
+        case 'TEXT':
+            return THEME.COLORS.TEXT;
+        default:
+            return THEME.COLORS.DEFAULT;
     };
 }
 
 export const getColorByState = (state: keyof typeof ButtonState) => {
-    
+
 }
