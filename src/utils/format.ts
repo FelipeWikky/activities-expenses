56aaec078b50562745a74572ea8a2fc13f24da5a
Felipe Lima <felipewikky@gmail.com>
2022-03-27 18:36:25 -0300

    Impement new logics to util functions

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,17 +1,28 @@
 import { format } from "date-fns";
 
 const FORMAT_DATE = "dd/MM/yyyy";
+const FORMAT_TIME = "HH:mm";
 const FORMAT_DATETIME = "dd/MM/yyyy HH:mm";
 
 export const formatDate = (value: string | Date) => {
-    if (value instanceof Date)
-        return format(value, FORMAT_DATE);
-    return format(new Date(value), FORMAT_DATE);
+    if(!value) return '';
+    if (value instanceof Date) return format(value, FORMAT_DATE);
+
+    const date = new Date(value) || '';
+    if (!date) return '';
+    return format(date, FORMAT_DATE);
+}
+export const formatTime = (value: string | Date) => {
+    if(!value) return '';
+
+    if (value instanceof Date) return format(value, FORMAT_TIME);
+    const date = new Date(value) || '';
+    if (!date) return '';
+    return format(date, FORMAT_TIME);
 }
 
-
 export const formatDateTime = (value: string | Date) => {
-    if(!value ) return '';
+    if (!value) return '';
     const date = value instanceof Date ? value : new Date(value);
     return format(date, FORMAT_DATETIME);
 }
--- src/utils/sort.ts
+++ src/utils/sort.ts
@@ -1,4 +1,5 @@
-export const sortByDate = (array: object[], property: string) => {
+export function sortByDate<T>(array: T[], property: keyof T) {
+    if(!array) return array;
     return array.sort((a, b) => {
         if (a[property] > b[property])  return -1;
         if (a[property] < b[property])  return 1;
