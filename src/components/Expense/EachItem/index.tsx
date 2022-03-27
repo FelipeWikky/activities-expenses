feb7c9e06798e2dbeae3eee8bfab3375915e589b
Felipe Lima <felipewikky@gmail.com>
2022-03-27 16:11:42 -0300

    Added more buttons on swipe

--- src/components/Expense/EachItem/index.tsx
+++ src/components/Expense/EachItem/index.tsx
@@ -1,28 +1,70 @@
 import { FontAwesome5 } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
 import Swipeable from 'react-native-gesture-handler/Swipeable';
 import { formatDateTime } from "../../../utils/format";
 import { Line } from "../../../layout/Line";
 
-import { Container, Title, Description, Checker, SwipableButton, DetailsButton, RemoveButton, CreatedAt } from "./styles";
+import {
+    Container, Title, Description, CreatedAt, Checker,
+    SwipableButton, DetailsButton, FinishButton, RemoveButton,
+} from "./styles";
 
 import { ItemProps } from "./types";
-import { useRef } from "react";
+import { useMemo, useRef } from "react";
 
 export const EachItem: React.FC<ItemProps> = ({ data, onSelectItem, onRemoveItem }) => {
     const ref = useRef<Swipeable>(null);
+
+    const LeftActions = useMemo(() => {
+        const buttons: JSX.Element[] = [];
+        if (!data.hasError) {
+            buttons.push(
+                <RemoveButton key={`error-${data.id}`} onPress={() => {
+                    ref?.current?.close();
+                }}>
+                    <MaterialIcons name="error" size={18} color="white" />
+                </RemoveButton>
+            )
+        }
+        if (!data.finished) {
+            buttons.push(
+                <FinishButton key={`finish-${data.id}`} onPress={() => {
+                    ref?.current?.close();
+                }}>
+                    <AntDesign name="checkcircle" size={18} color="white" />
+                </FinishButton>
+            );
+        }
+        if (buttons.length > 0) {
+            return (
+                <>
+                    {buttons.map(button => button)}
+                </>
+            );
+        }
+        return null;
+    }, [data, ref]);
+
+    const RightIcons = useMemo(() => {
+        return (
+            <RemoveButton onPress={() => {
+                if (onRemoveItem && data) onRemoveItem(data);
+                ref?.current?.close();
+            }}>
+                <FontAwesome5 name="trash" size={18} color="white" />
+            </RemoveButton>
+        );
+    }, [data, ref]);
+
     return (
         <Container>
             <SwipableButton
                 rightThreshold={60}
+                leftThreshold={60}
                 ref={ref}
-                renderRightActions={(_) => (
-                    <RemoveButton onPress={() => {
-                        if (onRemoveItem && data) onRemoveItem(data);
-                        ref?.current?.close();
-                    }}>
-                        <FontAwesome5 name="trash" size={20} color="white" />
-                    </RemoveButton>
-                )}
+                renderRightActions={(_) => RightIcons}
+                renderLeftActions={(_) => LeftActions}
             >
                 <DetailsButton
                     activeOpacity={0.5}
--- src/components/Expense/EachItem/styles.ts
+++ src/components/Expense/EachItem/styles.ts
@@ -4,16 +4,11 @@ import Swipeable from 'react-native-gesture-handler/Swipeable';
 import { CheckerProps, TextProps } from "./types";
 import { Button } from "../../Button";
 
-const SHADOW = {
-    1: "rgba(0, 0, 0, 0.1)",
-    3: "rgba(0, 0, 0, .3)"
-};
-
 export const Container = styled.View`
     border-top-width: 1px;
     border-bottom-width: 1px;
     border-style: solid;
-    border-color: ${({ theme }) => SHADOW[3]};
+    border-color: ${({ theme }) => theme.SHADOW[3]};
     margin-bottom: 12px;
     width: 100%;
     position: relative;
@@ -23,7 +18,7 @@ export const DetailsButton = styled.TouchableOpacity`
     width: 100%;
     justify-content: space-between;
     padding: 8px 4px;
-    max-height: 76px;
+    min-height: 76px;
 `;
 
 export const SwipableButton = styled(Swipeable)`
@@ -34,29 +29,39 @@ export const SwipableButton = styled(Swipeable)`
 `;
 
 export const RemoveButton = styled(Button).attrs(props => ({
-    empty: true
+    empty: true,
+    type: "DANGER"
 }))`
-    width: 60px;
-    background-color: ${({theme}) => theme.COLORS.DANGER}
+    width: 40px;
+    background-color: ${({ theme }) => theme.COLORS.DANGER}
+`;
+export const FinishButton = styled(Button).attrs(props => ({
+    empty: true,
+    type: "SUCCESS"
+}))`
+    width: 40px;
+    background-color: ${({ theme }) => theme.COLORS.SUCCESS}
 `;
 
-export const Title = styled.Text<TextProps>`
+export const Title = styled.Text.attrs(props => ({numberOfLines: 1}))<TextProps>`
     padding: 2px 2px 4px;
     text-decoration: ${({ finished }) => finished ? "line-through" : ""};
     font-family: ${({ theme }) => theme.FONTS.MEDIUM};
     font-size: ${({ theme }) => theme.SIZE.NORMAL_SMALL}px;
 `;
 
-export const Description = styled.Text<TextProps>`
+export const Description = styled.Text.attrs(props => ({numberOfLines: 1}))<TextProps>`
     padding: 4px 2px 2px;
     text-decoration: ${({ finished }) => finished ? "line-through" : ""};
     font-size: ${({ theme }) => theme.SIZE.SMALL}px;
+    /* max-width: 80%; */
+    max-height: 20px;
 `;
 
 export const Line = styled.View`
     height: 1px;
     width: 95%;
-    background-color: ${SHADOW[1]};
+    background-color: ${({ theme }) => theme.SHADOW[1]};
     align-self: center;
 `;
 
@@ -65,7 +70,7 @@ export const Checker = styled.View<CheckerProps>`
     width: 16px;
     position: absolute;
     border-radius: 10px;
-    border: 1px solid ${({ checked, error }) => error ? "red" : checked ? "green" : SHADOW[3]};
+    border: 1px solid ${({ checked, error, theme }) => error ? "red" : checked ? "green" : theme.SHADOW[3]};
     background-color: ${({ checked, error }) => error ? "red" : checked ? "green" : "transparent"};
     right: 4px;
     top: 4px;
