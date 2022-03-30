76f863ce3625b64653fa7b8e0b91ff50c2745bf0
Felipe Lima <felipewikky@gmail.com>
2022-03-29 23:21:40 -0300

    Added logic to check empty

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,16 @@
 export const getPercentageValue = (value: number, percentage: number) => {
     return value - ((value * percentage) / 100);
 }
+
+export const isEmpty = (obj: object) => {
+    let empty = true;
+    if(!obj || Object.keys(obj).length <= 0) return empty;
+
+    for(let key of Object.keys(obj)) {
+        if(!(obj[key] === null || obj[key] === undefined || obj[key] === false)) {
+            empty = false;
+            break;
+        }
+    }
+    return empty;
+}
