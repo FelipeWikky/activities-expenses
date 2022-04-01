4c972d967c9873825b1e19c55ff8ec68464ebfae
Felipe Lima <felipewikky@gmail.com>
2022-03-31 21:05:41 -0300

    Fix boolean validate on Box style

--- src/layout/Box/styles.ts
+++ src/layout/Box/styles.ts
@@ -4,6 +4,6 @@ import { BoxProps } from "./types";
 
 export const StyledBox = styled(Animated.View) <BoxProps>`
     flex-direction: ${({ direction }) => direction && direction === "row" ? "row" : "column"};
-    align-items: ${({ alignItems }) => alignItems? alignItems : "flex-start"};
-    justify-content: ${({ justifyContent }) => justifyContent? justifyContent : "flex-start"};
+    align-items: ${({ alignItems }) => !!(alignItems)? alignItems : "flex-start"};
+    justify-content: ${({ justifyContent }) => !!(justifyContent)? justifyContent : "flex-start"};
 `;
