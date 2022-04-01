8c5d11cda1000da0a91dcc788b036fdc02158a1b
Felipe Lima <felipewikky@gmail.com>
2022-03-31 21:05:02 -0300

    Added auth features

--- src/contexts/auth/index.tsx
+++ src/contexts/auth/index.tsx
@@ -1,5 +1,7 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useCallback, useEffect, useState } from "react";
+import { LocalStorage } from "../../services/storage/local";
 import { User } from "../../types/models/user";
+import { Constants } from "../../utils/constants";
 import { AuthContext as AuthContextType } from "./types";
 
 
@@ -8,11 +10,30 @@ const AuthContext = createContext<AuthContextType>({} as AuthContextType);
 export const AuthProvider: React.FC = ({ children }) => {
     const [currentUser, _setCurrentUser] = useState<User>();
 
-    
+    useEffect(() => {
+        LocalStorage.getItem<User>(Constants.STORAGE.AUTH)
+            .then(_setCurrentUser)
+    }, []);
+
+    const signin = useCallback(async (user: User, callback?: Function) => {
+        _setCurrentUser(user);
+        const saved = await LocalStorage.setItem(Constants.STORAGE.AUTH, user);
+        if(callback) callback();
+        return saved;
+    }, []);
+
+    const signout = useCallback((callback?: Function) => {
+        LocalStorage.removeItem(Constants.STORAGE.AUTH)
+            .then(() => {
+                if (callback) callback();
+            });
+    }, []);
+
     return (
         <AuthContext.Provider
             value={{
-                currentUser
+                currentUser,
+                signin, signout
             }}
         >
             {children}
--- src/contexts/auth/types.ts
+++ src/contexts/auth/types.ts
@@ -2,4 +2,7 @@ import { User } from "../../types/models/user";
 
 export type AuthContext = {
     currentUser?: User;
+
+    signin: (user: User, callback?: Function) => Promise<boolean>;
+    signout: (callback?: Function) => void;
 };
